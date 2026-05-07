f60dd6f630fc6dc66246d2524afe3dc1b8e2bce2
Lipbalm-jpg <kchan.software@gmail.com>
2026-05-06 21:02:17 -0700

    Iterate through cat data

--- static/cats.js
+++ static/cats.js
@@ -1,17 +1,29 @@
 console.log('{{ catsarray }}');
-var acatarray = document.getElementById("datadiv").dataset.catsarray;
+var acatarray = [];
 
 var datadiv = document.getElementById("datadiv");
 fetch("/catsdata")
 .then(res => res.json())
-.then(data => console.log(data));
+.then(data => { 
+    acatarray.push(...data)
+    console.log(acatarray);
 
-for (let i = 0; i < acatarray.length; i++){
-    let catdiv = document.createElement("div");
-    catdiv.innertext = acatarray[i];
-    let catbutton = document.createElement("button");
+    for (let i = 0; i < acatarray.length; i++){
+        let catdiv = document.createElement("p");
+        catdiv.textContent = acatarray[i][1];
+        let tupperwarediv = document.createElement("div");
+        let catbutton = document.createElement("button");
+        catbutton.onClick = () => {
+            localStorage.setItem("userID", this.dataset.id);
+        window.location.href = '../';
+        }
 
-    catbutton.dataset.id = i + 1;
-    datadiv.appendChild(catdiv);
-    datadiv.appendChild(catbutton);
+        catbutton.dataset.id = i + 1;
+        tupperwarediv.appendChild(catdiv);
+        datadiv.appendChild(tupperwarediv);
+        tupperwarediv.appendChild(catbutton);
+        tupperwarediv.style = "display:flex;";
+        console.log(acatarray[i][1]);
+    }
 }
+);
